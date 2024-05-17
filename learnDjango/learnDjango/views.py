6d8c0f55aa3ba5bd35e1bf9c8998c24b53d81212
amanavi75 <amanavi75@gmail.com>
2024-05-17 23:38:57 +0530

    added new route

--- learnDjango/learnDjango/views.py
+++ learnDjango/learnDjango/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render 
 
 
 def home(request):
-    return HttpResponse("hello , world you are at chai aur django home page ")
+   # return HttpResponse("hello , world you are at chai aur django home page ")
+   return render(request, 'index.html')
 def about(request):
     return HttpResponse("hello , world you are at chai aur django about page ")
-def contact(request):
+def contact(request): 
+    return HttpResponse("hello , world you are at chai aur django contact page ")
+def contact(request): 
     return HttpResponse("hello , world you are at chai aur django contact page ")
